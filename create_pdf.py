c4d63e17a9196344ffd3d4012c8d48cf85631dbb
onionj <onionj98@gmail.com>
2022-07-14 01:06:29 +0430

    update

--- create_pdf.py
+++ create_pdf.py
@@ -4,10 +4,10 @@ import pdfkit
 URL='https://onionj.github.io/cv/'
 
 options = {
-        'quiet':'',
-        'page-size':'A4',
-        'dpi':300,
-        'disable-smart-shrinking':'',
+        'margin-top': '0in',
+        'margin-right': '0.5in',
+        'margin-bottom': '0in',
+        'margin-left': '0.5in',
         }
 
 pdfkit.from_url(URL, 'cv.pdf', options=options, verbose=True)
